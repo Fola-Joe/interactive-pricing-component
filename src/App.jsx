00f78f7d96559c9e8ff8da459ddbcadc5ca73fc5
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-16 05:05:43 +0100

    update Pricing.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      
+      <h1>Simple, traffic-based pricing</h1>
+      <p>Sign-up for our 30-day trial. <br /> No credit card required.</p>
     </>
   )
 }
---
+++ src/Pricing.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Pricing() {
+    return (
+        <section>
+            <h1>Pageviews</h1>
+            <h2>$ /month</h2>
+            <p><span className="span-1">Monthly Billing</span> <span className="span2">Yearly Billing</span> </p>
+            <h3>Unlimited websites</h3>
+            <h3>100% data ownership</h3>
+            <h3>Email reports</h3>
+            <button>Start my trial</button>
+        </section>
+    )
+}
