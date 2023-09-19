0e8d6ad87def8bd3afbbd869659d514bc80dc761
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-19 03:24:23 +0100

    Update Pricing.jsx

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -1,31 +1,48 @@
 import './Pricing.css';
 import { useState } from 'react';
-// import InputSlider from './InputSlider';
+
 
 export default function Pricing() {
-    const [data, setData] = useState(0);
-    const updateValue = ((e) => setData(e.target.value));
+
+    const [views, setViews] = useState(10000);
+    const [price, setPrice] = useState(8);
+
+    const updateValue = (e) => {
+        const newViews = parseInt(e.target.value, 10); // Parse the input value to an integer
+        setViews(newViews); // Update the views state
+        if (newViews == 50000) {
+            setPrice(12);
+        } else if (newViews == 100000) {
+            setPrice(16);
+        } else if (newViews == 500000) {
+            setPrice(24);
+        } else if (newViews == 1000000) {
+            setPrice(36);
+        } else if (newViews == 10000) {
+            setPrice(8);
+        }
+    };
 
     return (
         <section className="main">
-            <h1 className="pricing-h1">{data} Pageviews</h1>
+            <h1 className="pricing-h1">{views.toLocaleString()} Pageviews</h1>
             <div className="slide-container">
                 <input type="range"
-                 min="1"
-                 max="100" 
-                 name="slider" 
-                 id="slider" 
-                 value={data} 
-                 className="slider" 
-                 onChange={updateValue}/>
+                    min="10000"
+                    max="1000000"
+                    step="10000"
+                    className="slider"
+                    name="slider"
+                    value={views}
+                    onChange={updateValue} />
             </div>
-            {/* <InputSlider /> */}
-            <h2>$ /month</h2>
+
+            <h2>${price}.00/month</h2>
             <p><span className="span-1">Monthly Billing</span> <span className="span2">Yearly Billing</span></p>
             <h3>Unlimited websites</h3>
             <h3>100% data ownership</h3>
             <h3>Email reports</h3>
             <button>Start my trial</button>
         </section>
-    )
+    );
 }
