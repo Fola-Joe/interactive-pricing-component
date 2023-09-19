d68c227c534295421412bbdd49637e8f783b8af6
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-19 01:12:16 +0100

    toggle slider
    Toggle slider design done. Updates number from 0-100 for now in real time.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
+import Pricing from './Pricing';
 
 function App() {
 
   return (
-    <>
-      <h1>Simple, traffic-based pricing</h1>
-      <p>Sign-up for our 30-day trial. <br /> No credit card required.</p>
-    </>
+    <section>
+      <section className='header'>
+        <h1 className='h1'>Simple, traffic-based pricing</h1>
+        <p>Sign-up for our 30-day trial. <br /> No credit card required.</p>
+      </section>
+      <Pricing />
+    </section>
   )
 }
 
--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -1,11 +1,27 @@
-
+import './Pricing.css';
+import { useState } from 'react';
+// import InputSlider from './InputSlider';
 
 export default function Pricing() {
+    const [data, setData] = useState(0);
+    const updateValue = ((e) => setData(e.target.value));
+
     return (
-        <section>
-            <h1>Pageviews</h1>
+        <section className="main">
+            <h1 className="pricing-h1">{data} Pageviews</h1>
+            <div className="slide-container">
+                <input type="range"
+                 min="1"
+                 max="100" 
+                 name="slider" 
+                 id="slider" 
+                 value={data} 
+                 className="slider" 
+                 onChange={updateValue}/>
+            </div>
+            {/* <InputSlider /> */}
             <h2>$ /month</h2>
-            <p><span className="span-1">Monthly Billing</span> <span className="span2">Yearly Billing</span> </p>
+            <p><span className="span-1">Monthly Billing</span> <span className="span2">Yearly Billing</span></p>
             <h3>Unlimited websites</h3>
             <h3>100% data ownership</h3>
             <h3>Email reports</h3>
