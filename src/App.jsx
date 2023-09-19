8df588a7ebfc9380866c41d7bddb2c6c46e1eaff
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-19 14:03:32 +0100

    text colors added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Pricing from './Pricing';
 function App() {
 
   return (
-    <section>
+    <section className="app-main">
       <section className='header'>
         <h1 className='h1'>Simple, traffic-based pricing</h1>
         <p>Sign-up for our 30-day trial. <br /> No credit card required.</p>
