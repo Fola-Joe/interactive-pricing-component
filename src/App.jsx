6f72ec252c9b9bb2b6e63073a8d9ce2526956e30
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-22 03:21:09 +0100

    Pricing component width adjustment
    Removed flexbox from body and used it in App.css.

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <section className="app-main">
-      <section className='header'>
-        <h1 className='h1'>Simple, traffic-based pricing</h1>
+      <section className="header">
+        <h1 className="h1">Simple, traffic-based pricing</h1>
         <p>Sign-up for our 30-day trial. <br /> No credit card required.</p>
       </section>
       <Pricing />
