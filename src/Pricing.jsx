586e7f3844defe262b56e8274f7a15b68c9691ce
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-25 06:47:56 +0100

    desktop view done

--- src/Pricing.jsx
+++ src/Pricing.jsx
@@ -16,29 +16,46 @@ export default function Pricing() {
 
     const [views, setViews] = useState(10000);
     const [price, setPrice] = useState(8);
+    const [isDiscounted, setIsDiscounted] = useState(false);
 
     const updateValue = (e) => {
         // Parse the input value to an integer
         const newViews = parseInt(e.target.value);
         // Update the views state
         setViews(newViews);
+        // updatePrice(newViews);
 
+        let newPrice = 8;
         if (newViews == 1000000) {
-            setPrice(36);
+            // setPrice(36);
+            newPrice = 36;
         } else if (newViews >= 500000) {
-            setPrice(24);
+            // setPrice(24);
+            newPrice = 24;
         } else if (newViews >= 100000) {
-            setPrice(16);
+            // setPrice(16);
+            newPrice = 16;
         } else if (newViews >= 50000) {
-            setPrice(12);
+            // setPrice(12);
+            newPrice = 12;
         } else if (newViews >= 10000) {
-            setPrice(8);
+            // setPrice(8);
+            newPrice = 8;
         }
+
+        // Apply the discount if the toggle switch is flipped
+        if (isDiscounted) {
+            newPrice *= 0.75;
+        }
+        setPrice(newPrice);
     };
 
     return (
         <section className="main">
-            <h1 className="pricing-h1">{formatLabel(views)} PAGEVIEWS</h1>
+            <div className="main-display">
+                <h1 className="pricing-h1">{formatLabel(views)} PAGEVIEWS</h1>
+                <h2 className="pricing-h2"><span className="priceValue">${isDiscounted ? (price * 0.75) : price}.00</span>/ month</h2>
+            </div>
             <div className="slide-container">
                 <input type="range"
                     min="10000"
@@ -50,17 +67,17 @@ export default function Pricing() {
                     onChange={updateValue} />
             </div>
 
-            <h2 className="pricing-h2"><span className="priceValue">${price}.00</span>/ month</h2>
+            <h2 className="pricing-h2"><span className="priceValue">${isDiscounted ? (price * 0.75) : price}.00</span>/ month</h2>
             <div className="billing-section">
                 <p className="mth-billing">Monthly Billing</p>
-                <ToggleSwitch />
+                <ToggleSwitch onChange={() => setIsDiscounted(isDiscounted)} />
                 <div className="discount">
                     <span className="yr-billing">Yearly Billing</span>
                     <span className="percentage percent-short">-25%</span>
                     <span className="percentage percent-long">-25% discount</span>
                 </div>
             </div>
-            <div>
+            <div className="footer">
                 <section className="bottom-section">
                     <section className="check-section">
                         <img src="./icon-check.svg" alt="" />
